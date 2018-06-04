a6b22e730419554e158b3723f4e7e1029e259a3c
oma-rika <omt2266@gmail.com>
2018-06-04 21:45:32 +0900

    2018/06/04

--- login/src/com/internousdev/login/action/LoginAction.java
+++ login/src/com/internousdev/login/action/LoginAction.java
@@ -1,9 +1,12 @@
 package com.internousdev.login.action;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.login.dao.LoginDAO;
+import com.internousdev.login.dto.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport implements SessionAware {
@@ -11,39 +14,37 @@ public class LoginAction extends ActionSupport implements SessionAware {
 		private String password;
 		private Map<String, Object> session;
 
-}
-public String execute() throws SQLException {
-	String ret = ERROR;
-	LoginDAO dao = new LoginDAO();
-	LoginDTO dto = new LoginDTO();
-	
-	dto = dao.select(name, password);
-	if(name.equals(dto.getName())){
-		if(password.equals(dto.getName())){
-			if(password.equals(dto.getPassword())){
-				ret = SUCCESS;
+		public String execute() throws SQLException {
+			String ret = ERROR;
+			LoginDAO dao = new LoginDAO();
+			LoginDTO dto = new LoginDTO();
+			
+			dto = dao.select(name, password);
+			if(name.equals(dto.getName())){
+				if(password.equals(dto.getPassword())){
+						ret = SUCCESS;
+				}
 			}
+			session.put("name", dto.getName());
+			return ret;
+
 		}
-		session.put("name", dto.getName());
-		return ret;
-	}
-}
-public String getName(){
-	return name;
-}
-public void setName(String name){
-	this.name = name;
-}
-public String getPassword(){
-	return password;
-}
-public void setPassword(String password){
-	this.password = password;
-}
-public Map<String, Object> getSession(){
-	return session;
-}
-public void setSession(Map<String, Object> session){
-	this.session = session;
-}
+		public String getName(){
+			return name;
+		}
+		public void setName(String name){
+			this.name = name;
+		}
+		public String getPassword(){
+			return password;
+		}
+		public void setPassword(String password){
+			this.password = password;
+		}
+		public Map<String, Object> getSession(){
+			return session;
+		}
+		public void setSession(Map<String, Object> session){
+			this.session = session;
+		}
 }
